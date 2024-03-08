2f2b1da35931461632aca5117f7a39be24c2e61e
Mohamed Yusuf <mohdgazyanii235@gmail.com>
2024-03-08 14:20:13 +0000

    Made some changes to the testJson file

--- cv-maker/src/main/java/io/github/cvmaker/service/CVService.java
+++ cv-maker/src/main/java/io/github/cvmaker/service/CVService.java
@@ -1,5 +1,11 @@
 package io.github.cvmaker.service;
 
+import com.aspose.tex.OutputFileSystemDirectory;
+import com.aspose.tex.TeXConfig;
+import com.aspose.tex.TeXJob;
+import com.aspose.tex.TeXOptions;
+import com.aspose.tex.rendering.PdfDevice;
+import com.aspose.tex.rendering.PdfSaveOptions;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
@@ -20,14 +26,29 @@ public class CVService {
 
     public void generateTex(CVParamsDTO cvParams, String templateName) throws IOException {
         Template template = this.freeMarkerConfiguration.getTemplate(templateName);
-        String outputFilePath = "src/main/resources/outputs/" + cvParams.getName() + ".tex";
+        String outputFilePath = "src/main/resources/outputs/" + cvParams.getName().replace(' ', '_') + ".tex";
         try (Writer writer = new FileWriter(outputFilePath)){
             template.process(cvParams, writer);
+            this.compileTexToPdf(outputFilePath);
         } catch (TemplateException e) {
             throw new RuntimeException(e);
         }
     }
 
+
+    public void compileTexToPdf(String texFilePath) {
+        try {
+            String command = String.format("pdflatex -output-directory=%s %s", "src/main/resources/outputs/", texFilePath);
+
+            Process process = Runtime.getRuntime().exec(command);
+            int exitCode = process.waitFor();
+            if (exitCode != 0) {
+                throw new IOException("pdflatex exited with error code: " + exitCode);
+            }
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException("Failed to compile TeX to PDF: " + e.getMessage(), e);
+        }
+    }
 }
 
 
